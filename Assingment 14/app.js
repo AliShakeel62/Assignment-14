05e8b929fe77faa65732bc5d3162281c4191de02
Ali Shakeel <148653842+AliShakeel62@users.noreply.github.com>
2024-01-06 02:02:34 +0500

    Add files via upload

---
+++ Assingment 14/app.js
@@ -0,0 +1,357 @@
+// cp1 
+// var a = alert("Ali");
+// var b = alert("Shakeel");
+// var c = alert("Alihakeel000@gmail.com");
+// var d = alert("0314002303");
+// var e = alert("Mare ko nahi pata");
+// var f = alert ("You are learn java script");
+// var g = alert ("Hey brother");
+
+
+//cp2 
+
+// var a =("myName");
+// var b =("");
+// var c = ("AssalamAlikum");
+// alert("teamName");
+// var bestman = "BaberAzam";
+
+// cp3 
+
+// var a = " CaseQty"
+// var CaseQty = "144";
+// console.log = (CaseQty);
+// var num = 9 ;
+// var result;
+// var  sumresult = 1+2;
+// var chair = 100; 
+// var tabel = 10; 
+// var Total = chair + tabel;
+// var b = 3;
+// var c = b+2;
+
+
+// //chp 4
+// var productcost = 3.45;
+// var nameOfBand; 
+// var legallyNamed = 2;
+// var name = "aliShakeel";
+// var 10hi;
+// var full name ;
+// var full!name;
+// var -name;
+
+// chp 5 
+
+// var a =454%4;
+// console.log (a);
+// var num = 20 % 6; 
+// console.log (num)
+// var largeNum = 1000* 2000;
+// console.log(largeNum);
+// var Hacker = 12;
+// var Boot =Hacker-3;
+// console.log(Boot)
+// var plan = 20%8;
+// console.log(plan)
+// var mul = 2*3;
+// var mul1 = alert(mul);
+
+// chp6 
+
+// var x =x++;
+// var x1 = 100;
+// var x2 = --x1;
+// console.log(x1,x2)
+// var x = 50; 
+// var y = x++;
+// console.log(y)
+// var y = 50; 
+// var z = --y;
+// console.log(y);
+// var a = 12;
+// var b = a--;
+// console.log(a,b)
+// var c = 50 ;
+// var d = ++c;
+// alert(c , d);
+
+// chp 7 
+// var calculatedNum = 2 + (2 * 6);
+// console.log(calculatedNum);
+// var calculatedNum = (2 + 2) * 6; 
+// console.log(calculatedNum);
+// var calculatedNum = (2 + 2) * (4 + 2); 
+// console.log(calculatedNum);
+// var calculatedNum = ((2 + 2) * 4) + 2; 
+// console.log(calculatedNum);
+// var result = (2 + 2) * (4 + 10);
+// console.log(result) 
+// var a = 2 + 2 * 4 + 10;
+// console.log(a)
+// var b = 4 / (2 * 4);
+// console.log(b)
+
+// chp8 
+
+// var num = "2" + "2"; 
+// console.log(num)
+// var  message = ("Hello," + "Dolly"); 
+// alert(message);
+// alert("33" + 3); 
+// console.log("the answer is 333")
+// var a =("Pakistan");
+// var b = (" Zindabad");
+// alert(a + b);
+// var q = "hey";
+// var r = 2 ;
+// console.log(q,r)
+
+// chp 9
+
+// var a = prompt("Enter your Name");
+// console.log(a);
+// var b = prompt ("Enter ypur Country name","China");
+// console.log(b);
+// var yourName = prompt ("Enter Your Name"); 
+// var c = prompt ("Enter ypur Country name","UK");
+// console.log(c);
+// var a = "Ali"
+// var b = " Shakeel"
+// var  c = prompt(a,b);
+// alert (c);
+
+// chp 10
+
+// var city = "Karachi";
+// if ((city = "Karachi")) {
+//   console.log("The City OF Lights");
+// }
+
+// var x = "Motorola";
+// var y = "Motorola";
+// var z = "Name";
+// if (x === y) {
+//   alert("Enter value of z");
+// }
+
+// var zipCode = prompt("Enter Zip Code");
+// if (zipCode === "10010") {
+//   alert("Karachi");
+// } else {
+//   alert("Please write correct city code");
+// }
+
+
+
+
+// var x = 1 ;
+// if (x == 1){
+//     var z = ++x;
+
+// }
+// console.log(x);
+// // Chp 10 (if statments)
+
+// var age =20;
+// if (age >= 18){
+//     console.log("Allow")
+// }
+
+// var bord1 = "student card";
+// var bord2 = "cnic";
+// if (bord1 == "student card" || bord2 == "cnic")
+// {
+//     var bord3 = prompt("Do you have bank account")
+// if( bord3 == "yes" ){
+//     console.log("congratulation")
+// }
+// };
+
+// //Chapter 11 (Comparison Operators)
+
+// var card1 = 40;
+// if (card1 !== "40" ){
+//     console.log("Sorry")
+// };
+
+// var wheather = 25 ;
+// if (wheather!== "25"){
+//     console.log("today wheather is "+wheather)
+// }
+
+// // Chp 12 (if...else and else if statements)
+
+// var Subject1 =parseFloat( prompt("Enter your Math Marks"));
+// var Subject2 = parseFloat(prompt("Enter your Math Marks"));
+// var Subject3 =parseFloat( prompt("Enter your Math Marks"));
+
+// var total = Subject1 + Subject2 + Subject3;
+// var persentage =(total/300)*100;
+// var grade;
+
+// if (persentage >= 85){
+//     grade="A";
+// }else if (persentage >= 75){
+//     grade="B";
+// }else if (persentage >= 65){
+//     grade="C";
+// }else {
+//     grade = "Fail";
+// }
+// console.log (total);
+// console.log (persentage);
+// console.log (grade);
+
+// // Chp 13 (Testing sets of conditions)
+
+// var firstNumber = 5;
+// var secondNumber = 10;
+
+// if (firstNumber <= secondNumber || firstNumber >= secondNumber) {
+//   alert("true");
+// }
+
+// // Chp 14 (If statements nested)
+
+// var window1 = 7;
+// var window2 = 7;
+
+// if (window1 === window2) {
+//     if (window1 <= window2) {
+//         alert("Must window1 id equal to window2 ");
+//     }
+// }
+// else{
+//     console.log("Congratulation")
+// }
+
+// Chp 15 (Array I)
+
+// brr =['a','b','c'];
+// var screen1 = brr[1];
+// console.log(brr);
+// console.log(screen1);
+
+// Chp 16 (Array II)
+
+// var arr = [1,2,3,4,5,6];
+// var maq = arr.pop();
+// console.log(arr)
+// console.log(maq)
+
+// var maq1 = arr.unshift("hey")
+// console.log (maq1)
+// console.log(arr)
+
+// Chp 16 (Array III)
+
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// var vets1 =pets.splice(1,2);
+// console.log(pets)
+
+// Chp 17 - 20 (for Loops)
+
+// var Firstname = ["Ali", "huzaifa", "Shayan", "Ayyan",];
+// var fox = prompt("Enter first name");
+
+// for (var i = 0; i < Firstname.length; i++) {
+//     if (Firstname == fox[i]) {
+//         console.log("welcome");
+//     }
+// }
+
+// if (i == Firstname.length) {
+//     alert("User not found. Please try again.");
+//   }
+
+//chp 21
+
+//var allLower = userInput.toLowerCase();
+// var x = "HEY";
+// x = x.toLowerCase();
+// console.log(x);
+
+// var cityName = "kaRacHi";
+// var a = cityName.slice(0,1).toUpperCase() + cityName.slice(1,7).toLowerCase();
+// console.log(a);
+// console.log(cityName);
+
+// chp 22 - 25 (Strings)
+
+
+// var Sameword = "Captain";
+// var Input = Sameword.slice(1,3);
+// console.log(Input);
+
+// var a = "hello";
+// var b = a.length;
+// console.log(b)
+
+// var a = "whether";
+// var b = "e";
+// if (a.length >= 3 && a[2] == b){
+//     console.log("pass")
+// }
+// else {
+//     console.log("sorry")
+// }
+
+
+// var a ="Hammad"
+// var arr = [];
+// for (var i=0 ; i <= a.length ; i++){
+//     arr[i] = a[i] 
+// }
+// console.log(arr)
+
+// var str = "1" ;
+// var newstr = str.replace("1","one")
+// console.log(newstr)
+
+
+// chp 26 
+
+// var a = 64.9;
+// var b  = Math.round(a);
+// console.log(b)
+
+// var a = .5;
+// var b = Math.floor(a);
+// console.log(b)
+
+// chp 27
+
+// var a = Math.random();
+// var b = Math.round(a*50)+1;
+// console.log(b)
+
+// var a = Math.random();
+// var b = Math.round(a*6);
+// console.log(b)
+
+// chp 28 - 29
+
+// var a = "5";
+// console.log(typeof a );
+// var b = Number(a);
+// console.log(typeof b );
+
+// var a = "3.14"
+// var b = parseInt(a);
+// console.log(b)
+
+
+// chp 30
+
+// var q = Math.random();
+// var a = q.toFixed(4);
+// console.log(q)
+// console.log(a)
+
+
+// var q = Math.random();
+// var a =q.toFixed(2);
+// alert(a);
+// console.log(q)
+// console.log(a)
